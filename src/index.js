209fa1fe321ac404ec72a3603335151eb68706e6
ChristianMendez1 <ChristianMendez1213@gmail.com>
2023-06-03 19:43:38 -0400

    Fullstack

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <StrictMode>
-    <Router>
+    <Router basename= '/Portfolio/'>
       <App />
     </Router>
   </StrictMode>,
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ export default function Home(){
     return(
         <div id='Home'>
             <h1 class='HomeText'>Christian Mendez's Portfolio</h1>
-            <h2 class='HomeText' id='DeveloperHome'>Front-End Developer</h2>
+            <h2 class='HomeText' id='DeveloperHome'>Full-Stack Developer</h2>
         </div>
     )
 }
